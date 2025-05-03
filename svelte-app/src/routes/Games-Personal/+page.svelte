f112249f1085292ee5ef6fd4d152e92b08ce0bd2
kacie franklin <C00299407@setu.ie>
2025-05-03 17:48:50 +0100

    Added content to personal games

--- svelte-app/src/routes/Games-Personal/+page.svelte
+++ svelte-app/src/routes/Games-Personal/+page.svelte
@@ -9,45 +9,43 @@
 <Header rectangle="HeaderImg.png" title="Personal"></Header>
 
 <div id="page-content">
-    <h1>Personal</h1>
 
     <div class=screenshotMain>
-
+        <img src="godIsDead.gif" alt="God Is Dead gif of battle between the sightless" height="600px" width="600px">
     </div>
 
     <div class="headerBox">
-
+        <h1>God Is Dead</h1>
+        <a href="https://github.com/KacieFranklin/GodIsDead">Github Link</a>
     </div>
 
     <div class="pageText">
-
+        <h2>                                          
+            God Is Dead is an work-in-progress GB, GBC and GBA game I have began to create using GB Studio. It is an RPG, Boss Rush, Roguelike where God, creator of all life, has fallen.
+            The creations, of this almighty being, fight in an tournament where the last one standing is crowned the new Creator of All.
+            The player will take control of an unnamed entity capable of rebirth, who, like the rest of the creations aim, to become the new God.
+        </h2>
     </div>
 
-    <div class="gifContent">
-
+    <div class=screenshotMainTwo>
+        <img src="balloonBlaster.png" alt="Vertical mockup with balloons coming down the screen" height="800px" width="600px">
     </div>
 
- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-
-    <div class=screenshotMain>
-
+    <div class="headerBoxTwo">
+        <h1>Balloon Blaster</h1>
+        <a href="https://github.com/KacieFranklin/BalloonBlaster">Github Link</a>
     </div>
 
-    <div class="headerBox">
-
-    </div>
-
-    <div class="pageText">
-
-    </div>
-
-    <div class="gifContent">
-
+    <div class="pageTextTwo">
+        <h2>Balloon Blaster is an work-in-progress arcade style game where you get points by shooting balloons as they descend the screen.
+            It is being created in Visual Studio using SFML and C++.
+            Different balloons will cause different effects when shot. Pink balloons give two hundred points.
+            Blue balloons give four hundred and gold ones give six hundred.
+            Minus ballons will take two hundred points and multiply balloons will multiply the score by two.
+            The game ends when the player shoots a skull balloon where they will be shown their score.
+        </h2>
     </div>
 
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <p>text</p>
 </div>
